6f6e6ba9c4708d31a5db143fb0003d85b320d734
UlisesCorp <38768959+UlisesCorp@users.noreply.github.com>
2018-06-07 08:41:17 -0300

    Add files via upload

---
+++ Ejercicio 4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (){
+	char car;
+	printf("Ingrese el caracter: ");
+	scanf(" %c", &car);
+
+	if (car >= 48 && car <= 60)
+		printf("El caracter ingresado es un número\n");
+	 else
+		printf("El caracter ingresado no es un número\n");
+	
+	return 0;
+}
